18854bdb086a249abebb73b65000ba88469b65b5
Tola's Terminal <sohbaker@gmail.com>
2018-12-18 14:04:57 +0000

    new test passed: has a player - o - when initialized

--- __tests__/player_test.js
+++ __tests__/player_test.js
@@ -7,8 +7,12 @@ describe('Player', () => {
     player = new Player;
   });
 
-    it('has a player - x - when initialized', () => {
-      expect(player.player1).toEqual('x')
-    });
+  it('has a player - x - when initialized', () => {
+    expect(player.player1).toEqual('x')
+  });
+
+  it('has a player - o - when initialized', () => {
+    expect(player.player2).toEqual('o')
+  });
 
 })
--- src/player.js
+++ src/player.js
@@ -1,8 +1,8 @@
 class Player {
   constructor() {
     this.player1 = 'x';
+    this.player2 = 'o';
   }
 }
-
 
 module.exports = { Player };
