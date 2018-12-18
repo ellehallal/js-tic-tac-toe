6fa13b4449a2d0b6a892c704bcb89f1d63d66a23
Tola's Terminal <sohbaker@gmail.com>
2018-12-18 10:54:03 +0000

    Test passing: can check whether player has won diagonally

--- __tests__/board_test.js
+++ __tests__/board_test.js
@@ -47,4 +47,13 @@ describe('Board', () => {
     board.putMarkOnGrid('x',8)
     expect(board.hasPlayerWon('x')).toEqual(true);
   });
+
+  it('can check whether player has won diagonally', () => {
+    board.putMarkOnGrid('o',1)
+    board.putMarkOnGrid('x',2)
+    board.putMarkOnGrid('o',5)
+    board.putMarkOnGrid('x',3)
+    board.putMarkOnGrid('o',9)
+    expect(board.hasPlayerWon('o')).toEqual(true);
+  });
 });
