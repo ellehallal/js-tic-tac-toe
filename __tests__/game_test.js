47a82e9e286ede5958416adc5c2b05161a181f00
Elle Hallal <daniellehallal@gmail.com>
2018-12-18 15:34:11 +0000

    new test passed: creates an instance of the player 1 & 2 when initialized

--- __tests__/game_test.js
+++ __tests__/game_test.js
@@ -11,4 +11,11 @@ describe('Game', () => {
     expect(game.grid).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
 
+  it('creates an instance of the player 1 when initialized', () => {
+    expect(game.player1).toEqual('x');
+  });
+
+  it('creates an instance of the player 2 when initialized', () => {
+    expect(game.player2).toEqual('o');
+  });
 })
--- src/game.js
+++ src/game.js
@@ -1,9 +1,13 @@
 import { Board } from '../src/board';
+import { Player } from '../src/player';
 
 class Game {
   constructor() {
     const board = new Board();
-    this.grid = board.grid
+    const player = new Player();
+    this.grid = board.grid;
+    this.player1 = player.player1;
+    this.player2 = player.player2;
   }
 }
 
