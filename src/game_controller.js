90ac46b1995a040a757d0de9248db36b37647646
Tola's Terminal <sohbaker@gmail.com>
2018-12-20 10:27:23 +0000

    additions to game controller class

--- src/game_controller.js
+++ src/game_controller.js
@@ -4,26 +4,28 @@ class GameController {
   constructor() {
     this.game = new Game;
     this.currentPlayerCanMakeMove = true;
+    this.gameIsActive = true;
   }
-}
 
 // start() {
-// }
-
-if(this.currentPlayerCanMakeMove === true) {
-  if(game.makeMove(currentPlayer, chosen_position) === true) {
-    this.currentPlayerCanMakeMove = false;
-  } else {
-    game.makeMove(currentPlayer, change_chosen_position);
+  while this.gameIsActive === true do {
+    if(this.currentPlayerCanMakeMove === true) {
+      if(game.makeMove(currentPlayer, chosen_position) === true) {
+        this.currentPlayerCanMakeMove = false;
+      } else {
+        game.makeMove(currentPlayer, change_chosen_position);
+      };
+    } else if(this.currentPlayerCanMakeMove === false) {
+      if(game.canContinuePlaying()) {
+        game.switchPlayer();
+        this.currentPlayerCanMakeMove = true;
+      } else {
+        this.gameIsActive = false;
+      }
+    }
   };
-
-  if(game.canContinuePlaying()) {
-    game.switchPlayer();
-    return (this.currentPlayerCanMakeMove = true);
-  }
-
-} else {
-    game.isTieOrIsWon();
-  }
+  game.isTieOrIsWon();
+// }
+}
 
 module.exports = { GameController };
