b8da1391e27b5ec5a90fd3cae73db4e67e87537b
Elle Hallal <daniellehallal@gmail.com>
2018-12-21 10:47:30 +0000

    tests passed: refactored game class to include dependency injection

--- __tests__/game_test.js
+++ __tests__/game_test.js
@@ -1,117 +1,83 @@
 import { Game } from '../src/game';
+import { Board } from '../src/board';
 
 describe('Game', () => {
-
-  let game;
-  beforeEach(() => {
-    game = new Game;
-  });
+  //
+  // let game;
+  // beforeEach(() => {
+  //   game = new Game;
+  // });
 
   it('creates an instance of the Board class when initialized', () => {
+    let board = new Board([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    let game = new Game(board)
     expect(game.grid).toEqual([1, 2, 3, 4, 5, 6, 7, 8, 9]);
   });
 
   it('creates an instance of the player 1 when initialized', () => {
+    let board = new Board([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    let game = new Game(board)
     expect(game.player1).toEqual('x');
   });
 
   it('creates an instance of the player 2 when initialized', () => {
+    let board = new Board([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    let game = new Game(board)
     expect(game.player2).toEqual('o');
   });
 
   it('sets the current player to player 1 when initialized', () => {
+    let board = new Board([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    let game = new Game(board)
     expect(game.currentPlayer).toEqual('x');
   });
 
   it('switches from one player to another', () => {
+    let board = new Board([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    let game = new Game(board)
     game.switchPlayer()
     expect(game.currentPlayer).toEqual('o');
   });
 
   it('allows a player to make a move', () => {
+    let board = new Board([1, 2, 3, 4, 5, 6, 7, 8, 9])
+    let game = new Game(board)
     game.makeMove(game.currentPlayer, 3)
     expect(game.grid).toEqual([1, 2, 'x', 4, 5, 6, 7, 8, 9]);
   });
-  //
+
   it('does not allow a player to make a move if the position is taken', () => {
-    game.makeMove(game.currentPlayer, 3)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 3)
+    let board = new Board([1, 2, 'x', 4, 'o', 6, 7, 8, 9])
+    let game = new Game(board)
     game.makeMove(game.currentPlayer, 5)
-    expect(game.grid).toEqual([1, 2, 'x', 4, 'o', 6, 7, 8, 9]);
-  });
-  //
-  it('knows the game has ended', () => {
     game.makeMove(game.currentPlayer, 1)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 2)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 3)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 5)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 4)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 7)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 6)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 9)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 8)
+    expect(game.grid).toEqual(['x', 2, 'x', 4, 'o', 6, 7, 8, 9]);
+  });
+
+  it('knows the game has ended', () => {
+    let board = new Board(["x", "x", "o", "o", "x", "x", "x", "o", "o"])
+    let game = new Game(board)
     expect(game.canContinuePlaying(game.currentPlayer)).toEqual(false);
   });
 
+
   it('knows the game can still be played', () => {
-    game.makeMove(game.currentPlayer, 1)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 2)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 3)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 5)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 4)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 7)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 6)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 9)
+    let board = new Board([1, 2, 'x', 4, 'o', 6, 7, 'x', 9])
+    let game = new Game(board)
     expect(game.canContinuePlaying(game.currentPlayer)).toEqual(true);
   });
 
   it('knows the game has ended and is a tie', () => {
-    game.makeMove(game.currentPlayer, 1)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 2)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 3)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 5)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 4)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 7)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 6)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 9)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 8)
+    let board = new Board(["x", "x", "o", "o", "x", "x", "x", "o", "o"])
+    let game = new Game(board)
+    game.canContinuePlaying();
     expect(game.isTieOrIsWon()).toEqual("Tie");
   });
 
   it('knows the game has ended and that a player has won', () => {
-    game.makeMove(game.currentPlayer, 5)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 3)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 2)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 4)
-    game.switchPlayer()
-    game.makeMove(game.currentPlayer, 8)
+    let board = new Board(["o", "x", 3, "o", "x", 6, "o", "x", "9"])
+    let game = new Game(board)
+    game.canContinuePlaying();
     expect(game.isTieOrIsWon()).toEqual("Win");
   });
 
