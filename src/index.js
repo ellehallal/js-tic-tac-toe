c4be5eeb2cbc66ef5811d0953525117f88640e35
Tola's Terminal <sohbaker@gmail.com>
2018-12-21 12:48:44 +0000

    removed console logs

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import { Game } from './game';
 import { Player } from './player';
 
 const board = new Board([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-console.log("HELOOOO")
 const game = new Game(board);
-console.log("Game", game)
 const gameController = new GameController(game);
-console.log("GameController", gameController)
 
 document.getElementById('test').innerHTML = board.grid;
