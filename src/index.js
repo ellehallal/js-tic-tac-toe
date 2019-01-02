d6d85c8e3b5c8e0e636cd4c4073185c0773f2ed6
Elle Hallal <daniellehallal@gmail.com>
2019-01-02 11:40:50 +0000

    Current player mark shows in position when clicked

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { Player } from './player';
 import { Game } from './game';
 import { GameController } from './game_controller';
 
+const board = new Board([1, 2, 3, 4, 5, 6, 7, 8, 9]);
+const game = new Game(board);
+const gameController = new GameController(game);
+
 const startButton = document.getElementById('start-button')
 const one = document.getElementById('one');
 const two = document.getElementById('two');
@@ -14,24 +18,18 @@ const seven = document.getElementById('seven');
 const eight = document.getElementById('eight');
 const nine = document.getElementById('nine');
 
+const positionOne = board.grid[0];
+const positionTwo = board.grid[1];
+const positionThree = board.grid[2];
+const positionFour = board.grid[3];
+const positionFive = board.grid[4];
+const positionSix = board.grid[5];
+const positionSeven = board.grid[6];
+const positionEight = board.grid[7];
+const positionNine = board.grid[8];
 
-
-
-const board = new Board([1, 2, 3, 4, 5, 6, 7, 8, 9]);
-const game = new Game(board);
-const gameController = new GameController(game);
 
 const showBoard = () => {
-  const positionOne = board.grid[0];
-  const positionTwo = board.grid[1];
-  const positionThree = board.grid[2];
-  const positionFour = board.grid[3];
-  const positionFive = board.grid[4];
-  const positionSix = board.grid[5];
-  const positionSeven = board.grid[6];
-  const positionEight = board.grid[7];
-  const positionNine = board.grid[8];
-
   one.innerHTML = positionOne;
   two.innerHTML = positionTwo;
   three.innerHTML = positionThree;
@@ -43,13 +41,53 @@ const showBoard = () => {
   nine.innerHTML = positionNine;
 }
 
-
-
 const startGame = () => {
-
   showBoard();
-
 }
 
 startButton.addEventListener("click", startGame)
-one.addEventListener("click", gameController.takeTurn(positionOne))
+
+one.addEventListener("click", function(){
+  gameController.takeTurn(1)
+  one.innerHTML = board.grid[0]
+});
+
+two.addEventListener("click", function(){
+  gameController.takeTurn(2)
+  two.innerHTML = board.grid[1]
+});
+
+three.addEventListener("click", function(){
+  gameController.takeTurn(3)
+  three.innerHTML = board.grid[2]
+});
+
+four.addEventListener("click", function(){
+  gameController.takeTurn(4)
+  four.innerHTML = board.grid[3]
+});
+
+five.addEventListener("click", function(){
+  gameController.takeTurn(5)
+  five.innerHTML = board.grid[4]
+});
+
+six.addEventListener("click", function(){
+  gameController.takeTurn(6)
+  six.innerHTML = board.grid[5]
+});
+
+seven.addEventListener("click", function(){
+  gameController.takeTurn(7)
+  seven.innerHTML = board.grid[6]
+});
+
+eight.addEventListener("click", function(){
+  gameController.takeTurn(8)
+  eight.innerHTML = board.grid[7]
+});
+
+nine.addEventListener("click", function(){
+  gameController.takeTurn(9)
+  nine.innerHTML = board.grid[8]
+});
